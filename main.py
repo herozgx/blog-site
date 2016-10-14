81dfcd5f5b1da1c1637af2228cfa96e489c846a9
herozgx <herozgx@126.com>
2016-10-14 19:30:07 -0400

    Add Flask Template

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
 
 app = Flask(__name__)   #__name__参数决定了程序的更目录
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return "hello world"
-
-@app.route('/<name>')
-def user(name):
-    return "hello {0}".format(name)
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
